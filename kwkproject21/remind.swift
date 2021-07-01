e7b23b0ad2597429ffe94945b1ad4bba796a38aa
Lavanya <lavanya.goel12@gmail.com>
2021-07-01 19:00:57 -0400

    remind screen

---
+++ kwkproject21/remind.swift
@@ -0,0 +1,14 @@
+//
+//  Remind.swift
+//  kwkproject21
+//
+//  Created by Lavanya Goel on 7/1/21.
+//
+
+import UIKit
+
+class reminds{
+    var name = ""
+    var category = ""
+    var date = Date()
+}
