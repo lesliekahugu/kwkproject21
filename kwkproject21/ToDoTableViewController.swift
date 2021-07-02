a23a164bbca03446df33a3f230df4c8a743779fd
Lavanya <lavanya.goel12@gmail.com>
2021-07-02 06:24:30 -0400

    reminder notification

--- kwkproject21/ToDoTableViewController.swift
+++ kwkproject21/ToDoTableViewController.swift
@@ -32,6 +32,33 @@ class ToDoTableViewController: UITableViewController {
       getToDos()
     }
     
+    
+    
+    @IBAction func testButton(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "ReminderTest"
+        content.body = "Category of reminder"
+        
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        let trigger = UNCalendarNotificationTrigger(
+                 dateMatching: dateComponents, repeats: false)
+        
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString,
+                    content: content, trigger: trigger)
+
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+           if error != nil {
+              // Handle any errors.
+           }
+        }
+    }
+    
+    
+    
 
     // MARK: - Table view data source
 
