b12a3c17db59038e38dd739e7d6f74fa68f9adc5
Lavanya <lavanya.goel12@gmail.com>
2021-07-02 05:19:37 -0400

    resources links added

--- kwkproject21/ResourcesViewController.swift
+++ kwkproject21/ResourcesViewController.swift
@@ -19,12 +19,50 @@ class ResourcesViewController: UIViewController {
       super.viewDidLoad()
      
     }
-    /*
+    
     @IBAction func buttonOne(_ sender: UIButton) {
-        if let url = URL(string: "https://www.google.com"){
+        if let url = URL(string: "https://suicidepreventionlifeline.org/"){
                     UIApplication.shared.open(url)
                 }
- */
+    }
+     @IBAction func bTwo(_ sender: UIButton) {
+         if let url = URL(string: " https://www.crisistextline.org/"){
+            UIApplication.shared.open(url)
+         }
+     }
+     @IBAction func bThree(_ sender: UIButton) {
+         if let url = URL(string: "https://screening.mhanational.org/screening-tools/"){
+                     UIApplication.shared.open(url)
+         }
+     }
+     @IBAction func bFour(_ sender: UIButton) {
+         if let url = URL(string: "https://www.massgeneral.org/psychiatry/guide-to-mental-health-resources/for-bipoc-mental-health"){
+                     UIApplication.shared.open(url)
+         }
+     }
+     @IBAction func bFive(_ sender: UIButton) {
+         if let url = URL(string: "https://mhanational.org/childrens-mental-health"){
+                     UIApplication.shared.open(url)
+         }
+     }
+     @IBAction func bSix(_ sender: UIButton) {
+         if let url = URL(string: "https://ok2talk.org/"){
+                     UIApplication.shared.open(url)
+         }
+     }
+     @IBAction func bSeven(_ sender: UIButton) {
+         if let url = URL(string: "https://teenlineonline.org/talk-now/"){
+                     UIApplication.shared.open(url)
+         }
+     }
+     @IBAction func bEight(_ sender: UIButton) {
+         if let url = URL(string: "https://www.thetrevorproject.org/get-help-now/"){
+                     UIApplication.shared.open(url)
+         }
+     }
+     
+    
+     
 }
 /*
     @IBAction func screeningButton(_ sender: UIButton) {
